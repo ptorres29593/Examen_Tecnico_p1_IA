c860eda43271c28674a85579a0ec78c91a43463d
ptorres29593 <123116819+ptorres29593@users.noreply.github.com>
2024-02-18 05:31:46 -0600

    Add is_prime function

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 # Main File
 
 # Imports
+import math
+
 
 # Here we declare variables
 
 # Here we start to write code for the functions
 
 # Function to check if a number is prime or not
+def is_prime(x):
+    if x % 2 == 0:
+        return False
+    upper_limit = int(math.sqrt(x) + 1)
+    for i in range(3, upper_limit, 2):
+        if x % i == 0:
+            return False
+    return True
 
 # Function to calculate fitness
 
