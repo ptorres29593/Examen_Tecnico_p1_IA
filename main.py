0198852f9a0ac3d1030c2ebff6b797d16257d9a4
ptorres29593 <123116819+ptorres29593@users.noreply.github.com>
2024-02-18 05:06:22 -0600

    add Instructions

---
+++ main.py
@@ -0,0 +1,23 @@
+# Main File
+
+# Imports
+
+# Here we declare variables
+
+# Here we start to write code for the functions
+
+# Function to check if a number is prime or not
+
+# Function to calculate fitness
+
+# Function to generate a random number
+
+# Function to crossover 2 parent numbers
+
+# Function to mutate numbers
+
+# Function to select parents via roulette
+
+# Function to select parents via elite specimens
+
+# Evolutionary Algorithm
