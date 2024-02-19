ad90d5877d374070271ef4b0273c4a5c576f6e9d
Ivancelot9 <134656635+Ivancelot9@users.noreply.github.com>
2024-02-18 23:04:51 -0600

    hice lo que pude

--- main.py
+++ main.py
@@ -1,89 +1,102 @@
-# Main File
+import random 
+#Generar numero al azar entre 1 - 1 millon
+def generate_random_number(lower_bound, upper_bound):
+    return random.randint(lower_bound, upper_bound)
 
-# Imports
-import math
-import random
-
-# Here we declare variables
-objective = 630300
-
-
-# Here we start to write code for the functions
-
-# Function to check if a number is prime or not
-def is_prime(num):
-    if num % 2 == 0:
-        return False
-    upper_limit = int(math.sqrt(num) + 1)
-    for i in range(3, upper_limit, 2):
-        if num % i == 0:
-            return False
-    return True
-
-
-# Function to calculate fitness
-def fitness(num):
-    if is_prime(num):
-        return abs(num - objective)
+#Buscar impar mas cercano
+def find_nearest_odd(number):
+    if number % 2 == 0:
+        return number - 1
     else:
-        return abs(num - objective) + 1000
+        return number
 
+#Mejor aptitud para evaluar la cercania de un numero a otro
+def fitness(target, number):
+    return abs(target - number)
 
-# Function to generate a random number
-def generate_random_number():
-    return random.randint(1, 1000000)
-
-
-# Function to crossover 2 parent numbers
-def crossover(parent1, parent2):
-    return random.randint(min(parent1, parent2), max(parent1, parent2))
-
-
-# Function to mutate numbers
-def mutate(num, mutation_rate):
-    if random.random() < mutation_rate:
-        return random.randint(num - 1000, num + 1000)
-
-
-# Function to select parents via roulette
+#Metodo Ruleta
 def roulette_wheel_selection(population, fitness_values):
     total_fitness = sum(fitness_values)
-    selection_probabilities = [fitness / total_fitness for fitness in fitness_values]
-    cumulative_probabilities = [sum(selection_probabilities[:i + 1]) for i in range(len(selection_probabilities))]
-    selected_parents = []
-    for _ in range(2):
-        random_value = random.random()
-        for i, cumulative_probability in enumerate(cumulative_probabilities):
-            if random_value <= cumulative_probability:
-                selected_parents.append(population[i])
-                break
-    return selected_parents
+    selection_probs = [fitness / total_fitness for fitness in fitness_values]
+    selected_individual = random.choices(population, weights=selection_probs)[0]
+    return selected_individual
+#Metodo Torneo
+def tournament_selection(population, fitness_values, tournament_size=3):
+    tournament_candidates = random.sample(list(zip(population, fitness_values)), tournament_size)
+    winner = min(tournament_candidates, key=lambda x: x[1])
+    return winner[0]
 
+#Metodo Elitista
+def elitist_selection(population, fitness_values, elite_percentage=0.1):
+    elite_size = max(1, int(len(population) * elite_percentage))
+    elite_indices = sorted(range(len(fitness_values)), key=lambda x: fitness_values[x])[:elite_size]
+    elite_population = [population[i] for i in elite_indices]
+    return random.choice(elite_population)
 
-# Function to select the closest prime number
-def elite_selection(population):
-    index = 0
-    aux = 1000000
-    for i, element in enumerate(population):
-        if is_prime(element):
-            if fitness(element) < aux:
-                index = i
-                aux = fitness(element)
-    return population[index]
+#Crossover (al ser numeros enteros lo que hace es separalos en digitos depende de la cantidad de digitos,
+#selecciona un punto para hacer el corte, despues el crossover para al final converitrlos en un valor entero)
+def crossover(parent1, parent2):
+    
+    parent1_digits = [int(digit) for digit in str(parent1)]
+    parent2_digits = [int(digit) for digit in str(parent2)]
+    
+    
+    crossover_point = random.randint(1, min(len(parent1_digits), len(parent2_digits)) - 1)
+    
+    
+    child1_digits = parent1_digits[:crossover_point] + parent2_digits[crossover_point:]
+    child2_digits = parent2_digits[:crossover_point] + parent1_digits[crossover_point:]
+    
+    
+    child1 = int("".join(map(str, child1_digits)))
+    child2 = int("".join(map(str, child2_digits)))
+    
+    return child1, child2
 
+#Funcion de algoritmo evolutivo
+def evolutionary_algorithm(target, population_size, max_generations, selection_method="roulette", mutation_rate=0.2, mutation_function=None):
+    population = [generate_random_number(1, 100000) for _ in range(population_size)]
+    generations = 0
+    while generations < max_generations:
+        fitness_values = [fitness(target, find_nearest_odd(individual)) for individual in population]
+        if 0 in fitness_values:  # Si ya encontramos un numero impar, terminamos
+            index = fitness_values.index(0)
+            return find_nearest_odd(population[index])
+        
+        # Seleccion de  los 3 metodos
+        if selection_method == "roulette":
+            parent1 = roulette_wheel_selection(population, fitness_values)
+            parent2 = roulette_wheel_selection(population, fitness_values)
+        elif selection_method == "tournament":
+            parent1 = tournament_selection(population, fitness_values)
+            parent2 = tournament_selection(population, fitness_values)
+        elif selection_method == "elitist":
+            parent1 = elitist_selection(population, fitness_values)
+            parent2 = elitist_selection(population, fitness_values)
+        
+        # Crossover
+        offspring1, offspring2 = crossover(parent1, parent2)
+        
+        # Mutacion
+        for i in range(len(population)):
+            if random.random() < mutation_rate and mutation_function:
+                population[i] = mutation_function(population[i])
+        
+        generations += 1
+    
+    # Si no encontramos un numero impar en el numero maximo de generaciones, retornamos el mejor candidato encontrado
+    best_individual_index = fitness_values.index(min(fitness_values))
+    return find_nearest_odd(population[best_individual_index])
 
-# Function to determine parents via tournament
-def tournament_selection(population):
-    best = elite_selection(population)
-    index = 0
-    aux = 0
-    for i, element in enumerate(population):
-        if is_prime(element):
-            if fitness(element) > aux:
-                index = i
-                aux = fitness(element)
-    worst = population[index]
-    return best, worst
+# Uso del algoritmo evolutivo
+if __name__ == "__main__":
+    target_number = generate_random_number(1, 1000000)
+    result = evolutionary_algorithm(target_number, population_size=10, max_generations=10, selection_method="roulette")
+    print("Numero objetivo:", target_number)
+    print("Numero impar mas cercano (ruleta):", result)
 
+    result = evolutionary_algorithm(target_number, population_size=10, max_generations=10, selection_method="tournament")
+    print("Numero impar mas cercano (torneo):", result)
 
-# Evolutionary Algorithm
+    result = evolutionary_algorithm(target_number, population_size=10, max_generations=10, selection_method="elitist")
+    print("Numero impar mas cercano (elitista):", result)
