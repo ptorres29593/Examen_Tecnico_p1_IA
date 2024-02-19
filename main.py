2d40a6187aaea85a196b1e692328f050c951e590
ptorres29593 <123116819+ptorres29593@users.noreply.github.com>
2024-02-19 12:29:42 -0600

    add evolutionary Algorithm v1.0

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import math
 import random
 
 # Here we declare variables
-objective = 630300
+objective = 500000
 
 
 # Here we start to write code for the functions
 
 # Function to check if a number is prime or not
 def is_prime(num):
+    if num is None:
+        return False
     if num % 2 == 0:
         return False
     upper_limit = int(math.sqrt(num) + 1)
@@ -23,6 +25,8 @@ def is_prime(num):
 
 # Function to calculate fitness
 def fitness(num):
+    if num is None:
+        return
     if is_prime(num):
         return abs(num - objective)
     else:
@@ -43,6 +47,7 @@ def crossover(parent1, parent2):
 def mutate(num, mutation_rate):
     if random.random() < mutation_rate:
         return random.randint(num - 1000, num + 1000)
+    return num
 
 
 # Function to select parents via roulette
@@ -61,7 +66,7 @@ def roulette_wheel_selection(population, fitness_values):
 
 
 # Function to select the closest prime number
-def elite_selection(population):
+def elite_selection(population, fitness_values):
     index = 0
     aux = 1000000
     for i, element in enumerate(population):
@@ -69,12 +74,14 @@ def elite_selection(population):
             if fitness(element) < aux:
                 index = i
                 aux = fitness(element)
+    if index == 0:
+        index = fitness_values.index(min(fitness_values))
     return population[index]
 
 
 # Function to determine parents via tournament
-def tournament_selection(population):
-    best = elite_selection(population)
+def tournament_selection(population, fitness_values):
+    best = elite_selection(population, fitness_values)
     index = 0
     aux = 0
     for i, element in enumerate(population):
@@ -82,8 +89,37 @@ def tournament_selection(population):
             if fitness(element) > aux:
                 index = i
                 aux = fitness(element)
+    if index == 0:
+        index = fitness_values.index(max(fitness_values))
     worst = population[index]
     return best, worst
 
 
 # Evolutionary Algorithm
+def evolutionary_algorithm(population_size, mutation_rate, max_generations):
+    population = [generate_random_number() for _ in range(population_size)]
+    generation_number = 1
+
+    for generation in range(max_generations):
+        fitness_values = [fitness(num) for num in population]
+        if min(fitness_values) == 0:
+            index = fitness_values.index(0)
+            return population, population[index], generation_number
+        new_population = []
+        elite = elite_selection(population, fitness_values)
+        new_population.append(elite)
+        parent1, parent2 = tournament_selection(population, fitness_values)
+        child1 = crossover(parent1, parent2)
+        new_population.append(mutate(child1, mutation_rate))
+        while len(new_population) < population_size:
+            parent3, parent4 = roulette_wheel_selection(population, fitness_values)
+            child2 = crossover(parent3, parent4)
+            new_population.append(mutate(child2, mutation_rate))
+        population = new_population
+        generation_number += 1
+    index = fitness_values.index(min(fitness_values))
+    return population, population[index], generation_number
+
+
+print(evolutionary_algorithm(10, 0.3, 100))
+
