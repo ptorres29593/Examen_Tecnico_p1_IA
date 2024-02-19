a27b1171d13a92a22000ff58abd660f5be5fcabb
Jennifer <jmartinez28728@ucq.edu.mx>
2024-02-19 13:16:53 -0600

    evolutionary algorithms

--- main.py
+++ main.py
@@ -1,90 +1,83 @@
-# Main File
-
-# Imports
-import math
 import random
 
-# Here we declare variables
-objective = 630300
-
-
-# Here we start to write code for the functions
-
-# Function to check if a number is prime or not
-def is_prime(num):
-    if num % 2 == 0:
+def is_prime(n):
+    if n <= 1:
         return False
-    upper_limit = int(math.sqrt(num) + 1)
-    for i in range(3, upper_limit, 2):
-        if num % i == 0:
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
+        i += 6
     return True
 
+def generate_initial_population(number, pop_size, max_offset=100):
+    population = []
+    for _ in range(pop_size):
+        offset = random.randint(-max_offset, max_offset)
+        candidate = number + offset
+        while not is_prime(candidate):
+            offset = random.randint(-max_offset, max_offset)
+            candidate = number + offset
+        population.append(candidate)
+    return population
 
-# Function to calculate fitness
-def fitness(num):
-    if is_prime(num):
-        return abs(num - objective)
-    else:
-        return abs(num - objective) + 1000
+def fitness(candidate, target):
+    return abs(candidate - target)
 
+def elite_selection(population, target, num_parents):
+    population.sort(key=lambda x: fitness(x, target))
+    return population[:num_parents]
 
-# Function to generate a random number
-def generate_random_number():
-    return random.randint(1, 1000000)
-
-
-# Function to crossover 2 parent numbers
-def crossover(parent1, parent2):
-    return random.randint(min(parent1, parent2), max(parent1, parent2))
-
-
-# Function to mutate numbers
-def mutate(num, mutation_rate):
-    if random.random() < mutation_rate:
-        return random.randint(num - 1000, num + 1000)
-    return num
-
-
-# Function to select parents via roulette
-def roulette_wheel_selection(population, fitness_values):
-    total_fitness = sum(fitness_values)
-    selection_probabilities = [fitness / total_fitness for fitness in fitness_values]
-    cumulative_probabilities = [sum(selection_probabilities[:i + 1]) for i in range(len(selection_probabilities))]
+def roulette_wheel_selection(population, target, num_parents):
+    total_inverse_fitness = sum(1 / (fitness(candidate, target) + 1) for candidate in population)
+    probabilities = [(1 / (fitness(candidate, target) + 1)) / total_inverse_fitness for candidate in population]
     selected_parents = []
-    for _ in range(2):
-        random_value = random.random()
-        for i, cumulative_probability in enumerate(cumulative_probabilities):
-            if random_value <= cumulative_probability:
-                selected_parents.append(population[i])
-                break
+    for _ in range(num_parents):
+        selected_parent = random.choices(population, probabilities)[0]
+        selected_parents.append(selected_parent)
     return selected_parents
 
 
-# Function to select the closest prime number
-def elite_selection(population):
-    index = 0
-    aux = 1000000
-    for i, element in enumerate(population):
-        if is_prime(element):
-            if fitness(element) < aux:
-                index = i
-                aux = fitness(element)
-    return population[index]
+def crossover(parents, num_offspring):
+    offspring = []
+    for _ in range(num_offspring):
+        parent1, parent2 = random.sample(parents, 2)
+        if len(str(parent1)) <= 1:  
+            crossover_point = 0  
+        else:
+            crossover_point = random.randint(1, len(str(parent1)) - 1)
+        child = int(str(parent1)[:crossover_point] + str(parent2)[crossover_point:])
+        offspring.append(child)
+    return offspring
 
+def mutation(offspring, mutation_rate, max_offset=10):
+    mutated_offspring = []
+    for child in offspring:
+        if random.random() < mutation_rate:
+            offset = random.randint(-max_offset, max_offset)
+            mutated_child = child + offset
+            while not is_prime(mutated_child):
+                offset = random.randint(-max_offset, max_offset)
+                mutated_child = child + offset
+            mutated_offspring.append(mutated_child)
+        else:
+            mutated_offspring.append(child)
+    return mutated_offspring
 
-# Function to determine parents via tournament
-def tournament_selection(population):
-    best = elite_selection(population)
-    index = 0
-    aux = 0
-    for i, element in enumerate(population):
-        if is_prime(element):
-            if fitness(element) > aux:
-                index = i
-                aux = fitness(element)
-    worst = population[index]
-    return best, worst
+def evolutionary_algorithm(target, pop_size=100, num_generations=100, num_parents=10, num_offspring=50, mutation_rate=0.1):
+    population = generate_initial_population(target, pop_size)
+    for generation in range(num_generations):
+        parents = roulette_wheel_selection(population, target, num_parents)
+        offspring = crossover(parents, num_offspring)
+        mutated_offspring = mutation(offspring, mutation_rate)
+        population = parents + mutated_offspring
+    best_solution = min(population, key=lambda x: fitness(x, target))
+    return best_solution
 
-
-# Evolutionary Algorithm
+target_number = 999900
+nearest_prime = evolutionary_algorithm(target_number)
+print(f"The nearest prime number to {target_number} is {nearest_prime}")
