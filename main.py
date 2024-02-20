d291ac8c705a8a9c8ca124b84fbfcd3f80e6302f
ptorres29593 <123116819+ptorres29593@users.noreply.github.com>
2024-02-19 19:49:32 -0600

    Evolutionary algorith v 2.5

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ objective = 200000
 
 # Function to check if a number is prime or not
 def is_prime(num):
-    if num % 2 == 0:
+    if num == 2:
         return True
+    if num % 2 == 0:
+        return False
     upper_limit = int(math.sqrt(num) + 1)
     for i in range(3, upper_limit, 2):
         if num % i == 0:
