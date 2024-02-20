5ab0184984bc19fd6a715bfa5b6acf41e63081f2
Ivancelot9 <134656635+Ivancelot9@users.noreply.github.com>
2024-02-19 19:17:03 -0600

    Mi codigo

--- main.py
+++ main.py
@@ -1,90 +1,58 @@
-# Main File
-
-# Imports
-import math
 import random
 
-# Here we declare variables
-objective = 630300
+# Define el número objetivo
+target_number = int(input("Ingresa el número:"))
 
-
-# Here we start to write code for the functions
-
-# Function to check if a number is prime or not
-def is_prime(num):
-    if num % 2 == 0:
+# Función para verificar si un número es primo
+def is_prime(n):
+    if n <= 1:
         return False
-    upper_limit = int(math.sqrt(num) + 1)
-    for i in range(3, upper_limit, 2):
-        if num % i == 0:
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
             return False
+        i += 6
     return True
 
+# Función para generar un número primo aleatorio dentro de un rango específico
+def generate_prime(min_value, max_value):
+    prime = random.randint(min_value, max_value)
+    while not is_prime(prime):
+        prime = random.randint(min_value, max_value)
+    return prime
 
-# Function to calculate fitness
-def fitness(num):
-    if is_prime(num):
-        return abs(num - objective)
-    else:
-        return abs(num - objective) + 1000
+# Función para seleccionar dos padres aleatorios
+def select_parents(population):
+    return random.sample(population, 2)
 
+# Función para ejecutar el algoritmo evolutivo
+def evolutionary_algorithm(population_size, max_generations):
+    population = [generate_prime(1, 1000000) for _ in range(population_size)]
+    generation_number = 0
 
-# Function to generate a random number
-def generate_random_number():
-    return random.randint(1, 1000000)
+    while generation_number <= max_generations:
+        parents = select_parents(population)
+        child1, child2 = parents  # No hay crossover ni mutación, ya que estamos tratando con primos
 
+        # Reemplazar los dos peores individuos de la población con los hijos
+        population.remove(max(population, key=lambda x: abs(x - target_number)))
+        population.remove(max(population, key=lambda x: abs(x - target_number)))
+        population.extend([child1, child2])
+        
+        # Comprobar si se ha encontrado una solución perfecta
+        if target_number in population:
+            return target_number, generation_number
 
-# Function to crossover 2 parent numbers
-def crossover(parent1, parent2):
-    return random.randint(min(parent1, parent2), max(parent1, parent2))
+        generation_number += 1
 
+    # Si no se encuentra una solución perfecta, se devuelve el número primo más cercano al objetivo
+    return min(population, key=lambda x: abs(x - target_number)), generation_number
 
-# Function to mutate numbers
-def mutate(num, mutation_rate):
-    if random.random() < mutation_rate:
-        return random.randint(num - 1000, num + 1000)
-    return num
-
-
-# Function to select parents via roulette
-def roulette_wheel_selection(population, fitness_values):
-    total_fitness = sum(fitness_values)
-    selection_probabilities = [fitness / total_fitness for fitness in fitness_values]
-    cumulative_probabilities = [sum(selection_probabilities[:i + 1]) for i in range(len(selection_probabilities))]
-    selected_parents = []
-    for _ in range(2):
-        random_value = random.random()
-        for i, cumulative_probability in enumerate(cumulative_probabilities):
-            if random_value <= cumulative_probability:
-                selected_parents.append(population[i])
-                break
-    return selected_parents
-
-
-# Function to select the closest prime number
-def elite_selection(population):
-    index = 0
-    aux = 1000000
-    for i, element in enumerate(population):
-        if is_prime(element):
-            if fitness(element) < aux:
-                index = i
-                aux = fitness(element)
-    return population[index]
-
-
-# Function to determine parents via tournament
-def tournament_selection(population):
-    best = elite_selection(population)
-    index = 0
-    aux = 0
-    for i, element in enumerate(population):
-        if is_prime(element):
-            if fitness(element) > aux:
-                index = i
-                aux = fitness(element)
-    worst = population[index]
-    return best, worst
-
-
-# Evolutionary Algorithm
+# Ejecutar el algoritmo evolutivo
+result, generation_number = evolutionary_algorithm(population_size=10, max_generations=100)
+print("Número primo más cercano al", target_number, "encontrado:", result)
+print("Número de generaciones:", generation_number)
